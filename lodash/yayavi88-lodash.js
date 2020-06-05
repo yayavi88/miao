0761404dd8d9bef7b025672a53a6c066d4d31f8c
huangya <709782986@qq.com>
2020-06-05 23:37:38 +0800

    change

--- lodash/yayavi88-lodash.js
+++ lodash/yayavi88-lodash.js
@@ -1,9 +1,31 @@
 var yayavi88 = {
-  chunk: function () { }
+  chunk: function (array, size) {
+    result = [];
+    arr = [];
+    if (array.length > size) {
+      arr = array.splice(0, size);
+      result.push(arr);
+    }
+    arr = array.slice(0);
+    result.push(arr);
+    return result;
+  },
 
 
 
+  flattenDeep: function (array) {
+    var result = [];
+    array.forEach(function (item) {
+      if (Array.isArray(array)) {
+        flattenDeep(item);
+      } else {
+        result.push(item);
+      }
+    });
+    return result;
+  },
 
-11
+
+
 
 }
