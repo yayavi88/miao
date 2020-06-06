6b03a131dae45e9ea71780b2c7db585220a6687d
huangya <709782986@qq.com>
2020-06-06 16:42:36 +0800

    update

--- lodash/yayavi88-lodash.js
+++ lodash/yayavi88-lodash.js
@@ -1,31 +1,153 @@
 var yayavi88 = {
-  chunk: function (array, size) {
-    result = [];
-    arr = [];
-    if (array.length > size) {
-      arr = array.splice(0, size);
-      result.push(arr);
-    }
-    arr = array.slice(0);
+  chunk,
+  compact,
+  concat,
+  flattenDeep,
+  before,
+  after,
+  ary,
+  unary,
+  flip,
+
+
+
+
+
+}
+
+
+function chunk(array, size) {
+  result = [];
+  arr = [];
+  if (array.length > size) {
+    arr = array.splice(0, size);
     result.push(arr);
-    return result;
-  },
+  }
+  arr = array.slice(0);
+  result.push(arr);
+  return result;
+}
+function compact(array) {
+  var result = [];
+  array.forEach(function (item) {
+    if (!(item == 'false' || item != item || item == 0 || item == 'null' || item == 'undefinde' || item == '')) {
+      result.push(item);
+    }
+  });
+  return result;
+}
+
+function concat(array, value) {
+  var result = [];
+  result.push(...array);
+  for (var i = 0; i < ary.length; i++) {
+    if (Array.isArray(...value)) {
+      result.push(...ary[i]);
+    } else {
+      result.push(ary[i]);
+    }
+  }
+  return result;
+}
 
 
+function flattenDeep(array) {
+  var result = [];
+  array.forEach(function (item) {
+    if (Array.isArray(array)) {
+      flattenDeep(item);
+    } else {
+      result.push(item);
+    }
+  });
+  return result;
+}
 
-  flattenDeep: function (array) {
-    var result = [];
-    array.forEach(function (item) {
-      if (Array.isArray(array)) {
-        flattenDeep(item);
-      } else {
-        result.push(item);
+function bind(f) {
+  var
+}
+
+//前n次调用原函数
+function before(n, func) {
+  var i = 0;
+  var result;
+  return function (...args) {
+    if (i < n) {
+      return result = func(...args);
+      i++;
+    }
+    else {
+      return result;
+    }
+  }
+}
+
+//调用n次之后调用原函数
+function after(n, func) {
+  var i = 0;
+  var result;
+  return function (...args) {
+    if (i >= n) {
+      return result = func(...args);
+      i++;
+    }
+  }
+}
+
+//调用时直接收n个参数
+function ary(func, n = func.length) {
+  return function (...args) {
+    return func(...args.slice(0, n))
+  }
+}
+
+//调用时直接一个参数
+function unary(func, n = func.length) {
+  return function (arg) {
+    return func(arg)
+  }
+}
+
+//翻转参数
+function flip(func) {
+  return function (...args) {
+    return func(...args.reverse());
+  }
+}
+
+//返回函数的取反值
+function negate(func) {
+  return function (...args) {
+    return !func(...args);
+  }
+}
+
+function spread(func) {
+  return function (ary) {
+    return func(...ary)
+  }
+}
+
+function bind(f, ...fixedArgs) {
+  return function (...args) {
+    var copy = fixedaArgs.slice();
+    var j = 0;
+    for (var i = 0; i < copy.length; i++) {
+      if (copy[i] == null) {
+        copy[i] = args[j];
+        j++;
       }
-    });
-    return result;
-  },
-
-
-
-
+      while (j < args.length) {
+        copy.push(args[j++]);
+      }
+    }
+    return f(...copy);
+  }
+}
+function fromPairs(pairs) {
+  var result = {};
+  for (var i = 0; i < pairs.length; i++) {
+    result[pairs[i][0]] = pairs[i][1];
+  }
+  return result;
 }
