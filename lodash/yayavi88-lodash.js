ba395e6cc7ac800989219ad179f8103f39185512
huangya <709782986@qq.com>
2020-06-08 20:21:49 +0800

    update

--- lodash/yayavi88-lodash.js
+++ lodash/yayavi88-lodash.js
@@ -27,11 +27,29 @@ var yayavi88 = {
   indexOf,
   initial,
   intersection,
-
-
-
-
-
+  join,
+  last,
+  nth,
+  pull,
+  pullAllWith,
+  pullAt,
+  remove,
+  reverse,
+  slice,
+  sortedIndex,
+  sortedIndexBy,
+  sortedLastIndexOf,
+  sortedIndexOf,
+  sortedLastIndex,
+  sortedLastIndexOf,
+  sortedUniq,
+  sortedUniqBy,
+  tail,
+  take,
+  takeRight,
+  takeRightWhile,
+  takeWhile,
+  union,
 
 
 
@@ -120,9 +138,9 @@ function fromPairs(pairs) {
   return result;
 }
 
-function bind(f) {
-  var
-}
+// function bind(f) {
+//   var
+// }
 
 //前n次调用原函数
 function before(n, func) {
@@ -234,19 +252,19 @@ function differenceWith(array, values, func) {
   return array;
 }
 
-function differenceBy(array, values, iteratee) {
-  if (typeof (iteratee) == "function") {
-    array.forEach(iteratee(item));
-    values.forEach(iteratee())
-  } else {
-    for (var j = 0; j < array.length; j++) {
-      if (func(array[j], ...values)) {
-        array.splice(j, 1);
-      }
-    }
-  }
-  return array;
-}
+// function differenceBy(array, values, iteratee) {
+//   if (typeof (iteratee) == "function") {
+//     array.forEach(iteratee(item));
+//     values.forEach(iteratee())
+//   } else {
+//     for (var j = 0; j < array.length; j++) {
+//       if (func(array[j], ...values)) {
+//         array.splice(j, 1);
+//       }
+//     }
+//   }
+//   return array;
+// }
 
 //判断相同
 function isEqual(value, other) {
@@ -368,12 +386,351 @@ function indexOf(array, value, fromIndex = 0) {
   }
   return -1
 }
-
+function lastIndexOf(array, value, fromIndex = array.length - 1) {
+  for (var i = fromIndex; i >= 0; i--) {
+    if (array[i] == value) {
+      return i;
+    }
+  } return -1;
+}
 //除去数组最后一个元素
 function initial(array) {
   return array.slice(0, array.length - 1);
 }
 //获取各数组的相同元素
-function intersection(...arrays) {
+// function intersection(...arrays) {
+//   var result = arrays[0];
+//   reduce(array, function (result, item) {
+//     for (var i = 0; i < result.length; i++) {
+//       for (var j = 0; j < item.length; j++) {
+//         if(result[i]==item[j]){
+
+//         }
+//       }
+//     }
+//   }, []);
+// }
+
+function join(array, separator = ',') {
+  var result = '';
+  for (var i = 0; i < array.length - 1; i++) {
+    result += array[i] + separator;
+  }
+  result += array[array.length - 1];
+  return result;
+}
+function last(array) {
+  return array[array.length - 1];
+}
+function nth(array, n = 0) {
+  if (n >= 0) {
+    return array[n];
+  } else {
+    return array[array.length + n];
+  }
+}
+//删除数组中的目标值
+function pull(array, ...values) {
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < values.length; j++) {
+      if (array[i] == values[j]) {
+        array.splice(i, 1);
+      }
+    }
+  }
+  return array;
+}
+
+function pullAll(array, values) {
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < values.length; j++) {
+      if (array[i] == values[j]) {
+        array.splice(i, 1);
+      }
+    }
+  }
+  return array;
+}
+
+function pullBy(array, values, iteratee) {
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < values.length; j++) {
+      if (iteratee(array[i]) == iteratee(values[j])) {
+        array.splice(i, 1);
+      }
+    }
+  }
+  return array;
+}
+
+function pullAllWith(array, values, comparator) {
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < values.length; j++) {
+      if (comparator(array[i], values[j])) {
+        array.splice(i, 1);
+      }
+    }
+  }
+  return array;
+}
+
+function pullAt(array, ...indexes) {
+  var result = [];
+  for (var i = 0; i < indexes.length; i++) {
+    result.push(array[indexes[i] - i]);
+    array.splice(indexes[i] - i, 1);
+  }
+  return result;
+}
+
+function remove(array, predicate) {
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    if (predicate(array[i])) {
+      result.push(array[i]);
+      array.splice(i, 1);
+      i--;
+    }
+  }
+  return result;
+}
+
+function reverse(array) {
+  var l = array.length;
+  for (var i = 0; i < Math.floor(l / 2); i++) {
+    var a;
+    a = array[i];
+    array[i] = array[l - i - 1];
+    array[l - i - 1] = a;
+  }
+  return array;
+}
+
+function slice(array, start = 0, end = array.length) {
+  var result = [];
+  for (var i = start; i < end; i++) {
+    result.push(array[i]);
+  }
+  return result;
+}
+
+function sortedIndex(array, value) {
+  for (var i = 0; i < array.length; i++) {
+    if (value <= array[i]) {
+      return i;
+    }
+  } return i;
+}
+
+// function sortedIndexBy(array, value, iteratee) {
+
+//   for (var i = 0; i < array.length; i++) {
+//     if (typeof (iteratee) == "function") {
+//       if (iteratee(value) < array[i]) {
+//         return i;
+//       }
+//     }
+
+//   } return i;
+// }
+function sortedIndexOf(array, value) {
+  for (var i = 0; i < array.length; i++) {
+    if (value == array[i]) {
+      return i;
+    }
+  }
+}
+
+function sortedLastIndex(array, value) {
+  for (var i = array.length - 1; i >= 0; i--) {
+    if (value >= array[i]) {
+      return i + 1;
+    }
+  }
+  return 0;
+}
+
+function sortedLastIndexOf(array, value) {
+  for (var i = array.length - 1; i >= 0; i--) {
+    if (array[i] == value) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+function sortedUniq(array) {
+  if (array.length <= 1) {
+    return [];
+  }
+  var result = [array[0]];
+  var j = 0;
+  for (var i = 1; i < array.length; i++) {
+    if (result[j] != array[i]) {
+      result.push(array[i]);
+      j++;
+    }
+  }
+  return result;
+}
+
+function sortedUniqBy(array, iteratee) {
+  if (array.length <= 1) {
+    return [];
+  }
+  var result = [array[0]];
+  var j = 0;
+  for (var i = 1; i < array.length; i++) {
+    if (iteratee(result[j]) != iteratee(array[i])) {
+      result.push(array[i]);
+      j++;
+    }
+  }
+  return result;
+}
+
+function tail(array) {
+  return array.slice(1);
+}
+
+function take(array, n = 1) {
+  if (n > array.length) {
+    n = array.length;
+  } else if (n == 0) {
+    return [];
+  }
+  return array.slice(0, n);
+}
+
+function takeRight(array, n = 1) {
+  if (n == 0) {
+    return [];
+  } else if (n >= array.length) {
+    return array;
+  }
+  return array.slice(array.length - n);
+}
+
+function takeRightWhile(array, predicate) {
+  var result = [];
+  for (var i = array.length - 1; i >= 0; i--) {
+    if (typeof (predicate) == "string") {
+      if (!(predicate in array[i])) {
+        result.unshift(array[i]);
+      }
+    } else if (typeof (predicate) == "function") {
+      if (predicate(array[i])) {
+        result.unshift(array[i]);
+      }
+    } else if (Array.isArray(predicate)) {
+      var count = 0;
+      for (var key in array[i]) {
+        if (predicate[0] in array[i] && (array[i][predicate[0]] == predicate[1])) {
+          count++;
+        }
+      }
+      if (Object.keys(array[i]).length == count) {
+        result.unshift(array[i]);
+      }
+    } else if (predicate.constructor === Object) {
+      var count = 0;
+      for (var key in predicate) {
+        if ((key in array[i]) && (predicate[key] == array[i][key])) {
+          count++;
+        }
+      } if (Object.keys(array[i]).length == count) {
+        result.unshift(array[i]);
+      }
+    }
+  }
+  return result;
+}
+
+function takeWhile(array, predicate) {
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    if (typeof (predicate) == "string") {
+      if (!(predicate in array[i])) {
+        result.push(array[i]);
+      }
+    } else if (typeof (predicate) == "function") {
+      if (predicate(array[i])) {
+        result.push(array[i]);
+      }
+    } else if (Array.isArray(predicate)) {
+      var count = 0;
+      for (var key in array[i]) {
+        if (predicate[0] in array[i] && (array[i][predicate[0]] == predicate[1])) {
+          count++;
+        }
+      }
+      if (Object.keys(array[i]).length == count) {
+        result.push(array[i]);
+      }
+    } else if (predicate.constructor === Object) {
+      var count = 0;
+      for (var key in predicate) {
+        if ((key in array[i]) && (predicate[key] == array[i][key])) {
+          count++;
+        }
+      } if (Object.keys(array[i]).length == count) {
+        result.push(array[i]);
+      }
+    }
+  }
+  return result;
+}
+
+function union(...arrays) {
+  var ary = [...arrays[0]];
+  var result = {};
+  for (var e = 0; e < ary.length; e++) {
+    result[ary[e]] = 1;
+  }
+  for (var i = 1; i < arrays.length; i++) {
+    for (var j = 0; j < arrays[i].length; j++) {
+      if (!(arrays[i][j] in result)) {
+        result[arrays[i][j]] = 1;
+      }
+    }
+  }
+  for (var key in result) {
+
+  }
+  return Object.keys(result).map(Number);
+}
+
+function unionBy(...arrays, iteratee) {
+  var ary = [iteratee(...arrays[0])];
+  var result = {};
+  for (var e = 0; e < ary.length; e++) {
+    result[ary[e]] = 1;
+  }
+  for (var i = 1; i < arrays.length; i++) {
+    for (var j = 0; j < arrays[i].length; j++) {
+      if (!(arrays[i][j] in result)) {
+        result[arrays[i][j]] = 1;
+      }
+    }
+  }
+  for (var key in result) {
+
+  }
+  return Object.keys(result).map(Number);
+}
+
+function union(...arrays) {
+  var result = [...arrays[0]];
+  for (var i = 0; i < arrays.length; i++) {
+    reduce(arrays, function (result, arrays[i], idx) {
+      for (var i = 0; i < result.length; i++) {
+
+      }
+    }, result);
+  }
 
 }
